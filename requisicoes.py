496fe380c34f4ebf77264d66bf154a302f152186
lukasmachado-elysia <lukas.machado@elysia.com.br>
2022-10-21 13:58:09 -0300

    update v2

--- requisicoes.py
+++
@@ -1,7 +0,0 @@
-import requests
-import pandas as pd
-
-basic = requests.get('https://elysia.zeev.it/api/2/assignments', headers={'Authorization': 'Bearer HihrPwM6iplpMc20YafyI0X20hYBwMJjn9O5b3uRXLc%3D'})
-df = pd.DataFrame(basic.json())
-print(df)
-
