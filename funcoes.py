be5af61c60be877c6b1df6a9f684a9342e4fb64a
lukasmachado-elysia <lukas.machado@elysia.com.br>
2022-11-10 13:57:40 -0300

    ...

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,10 @@
 import requests
+import os
+from datetime import datetime, timedelta
+from dateutil import parser
+from flask import Flask, render_template
+from flask_mail import Mail, Message
+
 
 def printError(e):
     print('\n------------------------------------') 
@@ -57,3 +63,155 @@ def requests_API_Orquestra(metodo='GET',urlAcesso="https://elysia.zeev.it",tipoA
     except Exception as e:
         print('funcoes')
         printError(e)
+
+def cria_log(msgExecucoes:str,nomePrograma:str):
+    """
+        Funcao
+        ------
+            Cria um log de execucao do programa no local: `absolutePathPrograma\\logsPrograma\\log_data_nomePrograma.log`
+        
+        Parametros
+        ----------
+            msgExecucoes : str, obrigatorio
+                Mensagem de execucao.\n
+                * O recomendado eh armazenar em uma string cada mensagem de execucao que voce deseja adicionar no log e no final chamar esta funcao.
+            
+            nomePrograma : str, obrigatorio
+                Nome do programa que sera colocado no log.
+   
+        Retorno
+        -------
+            bool : retorna `True` se o log foi criado com sucesso e retorna `False` em caso de excecao ou problema na criacao do arquivo.
+    """
+    try:
+        logMsg = '''-------------------------------------------\n------- LOG DE EXECUCAO DO PROGRAMA -------\n-------------------------------------------\nData e Hora: {0}\nLog:\n>> {1}\n-------------------------------------------\n\n'''.format(datetime.today().strftime("%d/%m/%Y %Hh:%Mm:%Ss"), msgExecucoes)
+        nomePrograma = "teste"
+        absPathFolder = os.getcwd() + "\\logsPrograma\\"
+
+        # Criacao da pasta
+        if not(os.path.exists(absPathFolder)):
+            # Pasta nao existe
+            print(">> Pasta: {} nao existe!".format(absPathFolder))
+            print(">> Criando pasta...")
+
+            os.mkdir(absPathFolder)
+            print(">> Pasta criada!")
+        else:
+            # Pasta existe
+            print(">> Pasta: {} existe!".format(absPathFolder))
+
+        # Criacao do arquivo
+        nomeArquivo =  "log_{}_{}.txt".format(datetime.today().strftime("%d_%m_%Y"), nomePrograma)
+        print(">> Verificando se {} existe".format(nomeArquivo))
+
+        if not(os.path.exists(absPathFolder + nomeArquivo)):
+            # Arquivo nao existe, cria ele e alimenta
+            print(">> Nao existe!")
+            print(">> Criando arquivo...")
+
+            with open(absPathFolder + nomeArquivo, 'w', encoding="utf-8") as file:
+                print(">> Escrevendo no arquivo...")
+                writeData = file.write(logMsg)
+                print(">> writeData: {}".format(str(writeData)))
+
+            # Fecha arquivo
+            print(">> Fechando arquivo...")
+            file.close()
+            if file.closed:
+                print(">> Arquivo fechado!")
+                return absPathFolder, nomeArquivo, True
+            else: 
+                print(">> Erro ao fechar o arquivo!\n<!!!> Possivel problema na criacao do log <!!!>")
+                return '', '', False
+        else:
+            # Arquivo ja existe, possivelmente uma nova execucao
+            print(">> Arquivo existe!")
+
+            with open(absPathFolder + nomeArquivo, 'a', encoding="utf-8") as file:
+                print(">> Escrevendo no arquivo...")
+                appendData = file.write(logMsg)
+                print(">> appendData: {}".format(str(appendData)))
+
+            # Fecha arquivo
+            print(">> Fechando arquivo...")
+            file.close()
+            if file.closed:
+                print(">> Arquivo fechado!")
+                return absPathFolder, nomeArquivo, True
+            else: 
+                print(">> Erro ao fechar o arquivo!\n<!!!> Possivel problema na criacao do log <!!!>")
+                return '', '', False
+    except Exception as e:
+        printError(e)
+        return '', '', False
+
+def envia_Email(enderecosEnvio:list, assunto:str, mensagem:str, template:str,listAttach:list=[]) -> str:
+    '''
+        Funcao
+        ------
+
+            Envia e-mail para os enderecos de envio especificados na lista.
+            * O e-mail `bot.elysia@gmail.com` eh o remetente.
+    
+        Parametros
+        ----------
+            enderecosEnvio : list, obrigatorio
+                Uma lista dos enderecos de e-mail que receberao o e-mail.
+            
+            assunto : str, obrigatorio
+                O assunto que ira no e-mail.
+            
+            mensagem : str, obrigatorio
+                A mensagem que sera colocada no corpo do e-mail.
+            
+            template : str, obrigatorio
+                Nome do `template.html` que sera utilizado como base para a mensagem
+                
+            listAttach : list, opcional
+                Uma lista composta por sublistas com os anexos: `[ ["caminhoAnexo", "nomeAnexo", "tipoAnexo"], ..., n-sublistAttach ]`.\n\n 
+                Para o `tipoAnexo`, verificar documentacao do MDN:\n
+                *`https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types` 
+                
+        Retorno
+        -------
+            Retorna uma `string` especificando se o envio foi realizado com sucesso ou nao.
+    '''
+    try:
+        # Dados do email e flask
+        password = 'xeppbwvbemvrpgms'
+        app  = Flask(__name__)
+        mail = Mail(app)
+        
+        print(">> Funcao de envio automatico de e-mail v1.0 <<\n")
+        print(">> Conectando ao servidor do Gmail...")
+
+        # Configurando conexao com servidor de e-mail
+        app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+        app.config['MAIL_PORT'] = 465
+        app.config['MAIL_USERNAME'] = 'bot.elysia@gmail.com'
+        app.config['MAIL_PASSWORD'] = password
+        app.config['MAIL_USE_TLS'] = False
+        app.config['MAIL_USE_SSL'] = True
+        mail = Mail(app)
+
+        print(">> Conectado com sucesso!")
+        print(">> Montando e-mail...")
+
+        with app.app_context():
+            msg = Message(subject=assunto, sender=("Elysia Chlorotica Bot", "bot.elysia@gmail.com"), recipients=enderecosEnvio)
+            #msg.body = mensagem
+            msg.html = render_template(template,data=mensagem)
+            # Se for para enviar com attachment
+            if len(listAttach) == 0:
+                print(">> Sem anexo para envio...")
+            else:
+                print(">> Anexando documentos...")
+                for vals in listAttach:
+                    with app.open_resource(vals[0] + vals[1]) as fp:
+                        msg.attach(vals[1], vals[2], fp.read())
+            print(">> Enviando e-mail...")
+            mail.send(msg)
+        return ">> E-mail enviado com sucesso para (" + ", ".join(enderecosEnvio) + ") no dia {0}!".format(datetime.today().strftime("%d/%m/%Y as %Hh:%Mm:%Ss"))
+    except Exception as e:
+        printError(e)
+        return ">> Nao foi possivel enviar o email!"
